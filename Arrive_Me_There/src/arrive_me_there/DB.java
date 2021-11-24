31a1d5f1e352c928c68f39a00063feef2672679d
Mohamed Adel <youssefadel99999@gmail.com>
2021-11-24 17:56:08 +0200

    DB Class

---
+++ Arrive_Me_There/src/arrive_me_there/DB.java
@@ -0,0 +1,105 @@
+package arrive_me_there;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class DB {
+
+    private static DB instance;
+    private ArrayList<Client> clientList;
+    private ArrayList<Admin> adminList;
+    private ArrayList<Driver> driverList;
+    private ArrayList<Ride> rideList;
+
+    private DB() {
+        clientList = new ArrayList<Client>();
+        adminList = new ArrayList<Admin>();
+        driverList = new ArrayList<Driver>();
+        rideList = new ArrayList<Ride>();
+    }
+
+    public static DB getInstance() {
+        if (instance == null) {
+            instance = new DB();
+        }
+        return instance;
+    }
+
+    public void addClient(Client client) {
+        clientList.add(client);
+
+    }
+
+    public void addAdmin(Admin admin) {
+        adminList.add(admin);
+    }
+
+    public void addDriver(Driver driver) {
+        driverList.add(driver);
+    }
+
+    public void addRide(Ride ride) {
+        rideList.add(ride);
+    }
+    
+    public ArrayList<Client> getClientList() {
+        return clientList;
+    }
+
+    public ArrayList<Admin> getAdminList() {
+        return adminList;
+    }
+
+    public ArrayList<Driver> getDriverList() {
+        return driverList;
+    }
+
+    public ArrayList<Ride> getRideList() {
+        return rideList;
+    }
+
+    public Client getClient(String mobileNumber) {
+        for (int i = 0; i < clientList.size(); i++) {
+            if (mobileNumber.equals(clientList.get(i).getInfo().getMobileNumber())) {
+                return clientList.get(i);
+            }
+        }
+        return null;
+    }
+
+    public Admin getAdmin(String mobileNumber) {
+        for (int i = 0; i < adminList.size(); i++) {
+            if (mobileNumber.equals(adminList.get(i).getInfo().getMobileNumber())) {
+                return adminList.get(i);
+            }
+        }
+        return null;
+    }
+
+    public Driver getDriver(String mobileNumber) {
+
+        for (int i = 0; i < driverList.size(); i++) {
+            if (mobileNumber.equals(driverList.get(i).getinfo().getMobileNumber())) {
+                return driverList.get(i);
+            }
+        }
+        return null;
+
+    }
+    
+//    public Driver getDriver(String mobileNumber) {
+//        for (Driver driver : driverList)
+//            if (driver.getInfo().getMobileNumber() == mobileNumber)
+//                return driver;
+//        return null
+//    }
+    
+    public Ride getRide (int id){
+        for (Ride ride : rideList){
+            if (ride.getID == id)
+                return ride;
+        }
+        return null;
+    }
+
+}
