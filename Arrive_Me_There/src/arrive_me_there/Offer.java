49c399930e79f4b71c32870ca6710367c68fe7c8
Mohamed Adel <youssefadel99999@gmail.com>
2021-11-24 18:32:19 +0200

    Offer & Ride mofified

--- Arrive_Me_There/src/arrive_me_there/Offer.java
+++ Arrive_Me_There/src/arrive_me_there/Offer.java
@@ -1,23 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package arrive_me_there;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Dell
- */
+
 public class Offer {
+    public Offer (Driver driver, int price){
+    this.driver = driver;
+    this.price = price;
+    }
     private int price;
-   // private Driver driver;
-    Offer add(Offer offer){
-    Offer of=new Offer();
-    of.price=offer.price;
-    return of;
+    private Driver driver;
+    public void setPrice(int price){
+        this.price = price;
+    }
+    
+    public int getPrice(){
+        return price;
+    }
+    
+    public void setDriver (Driver driver){
+        this.driver = driver;
+    }
+    public Driver getDriver (){
+        return driver;
     }
     
 }
--- Arrive_Me_There/src/arrive_me_there/Ride.java
+++ Arrive_Me_There/src/arrive_me_there/Ride.java
@@ -12,52 +12,56 @@ import java.util.ArrayList;
  * @author Dell
  */
 public class Ride {
+
     private int id;
     private String source;
     private String destination;
     private Client client;
     private boolean isCompleted;
     private ArrayList<Offer> offerList;
-     
-    public void RideDetails(String Source,String Destination){
-    
-    this.source=source;
-    this.destination=destination;
-    
-    }
-    public String getsource(){
-        
-       return source;
-       
-    }
-    public String getdestination(){
-        
-       
-       return destination;
-    }
-    
-    public static void addOffer(Offer offer){
-    
-    Offer input=new Offer();
-    Offer off=input.add(offer);
-    ArrayList<Offer> offerList=new  ArrayList<Offer>();
-    offerList.add(off);
-    
-    }
-    
-    
 
-   public void completed(boolean ride_finished){
-   
-   isCompleted=ride_finished;
-   
-   }
-   public boolean ifcompleted(){
-    return isCompleted;
-    
-    
+    public void RideDetails(String Source, String Destination) {
+
+        this.source = source;
+        this.destination = destination;
+
+    }
+    public Client getClient (){
+        return client;
+    }
+    public String getsource() {
+
+        return source;
+
     }
 
+    public String getdestination() {
+
+        return destination;
+    }
+
+    public ArrayList<Offer> getOfferList(){
+        return offerList;
+    }
     
-    
+    public static void addOffer(Offer offer) {
+
+        Offer input = new Offer();
+        Offer off = input.add(offer);
+        ArrayList<Offer> offerList = new ArrayList<Offer>();
+        offerList.add(off);
+
+    }
+
+    public void completed(boolean ride_finished) {
+
+        isCompleted = ride_finished;
+
+    }
+
+    public boolean ifcompleted() {
+        return isCompleted;
+
+    }
+
 }
