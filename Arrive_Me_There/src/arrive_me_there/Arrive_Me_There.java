147837cfe951aff1f7035a3f123d68eac2ba957c
Mo7amed676 <84755046+Mo7amed676@users.noreply.github.com>
2021-11-24 17:46:38 +0200

    Update Arrive_Me_There.java

--- Arrive_Me_There/src/arrive_me_there/Arrive_Me_There.java
+++ Arrive_Me_There/src/arrive_me_there/Arrive_Me_There.java
@@ -10,19 +10,32 @@ import java.util.Scanner;
 public class Arrive_Me_There {
 
     public static void main(String[] args) {
-        System.out.println("Login as 1-Admin\n         2-Client\n         3-Driver\n         4-exit");
+        System.out.println("Login For Initial Admin.");
+
+        Scanner inp1 = new Scanner(System.in);
+        Scanner inp2 = new Scanner(System.in);
+        Scanner inp3 = new Scanner(System.in);
+        Scanner inp4 = new Scanner(System.in);
+        System.out.print("enter your name :");
+        String userName1 = inp1.next();
+        System.out.print("enter your password :");
+        String password1 = inp2.next();
+        System.out.print("enter your email :");
+        String email1 = inp3.next();
+        System.out.print("enter your phone number :");
+        String phoneNumber1 = inp4.next();
+        System.out.println("Login as 1-Add New Admin\n         2-Client\n         3-Driver\n         4-exit");
         UserPersonalInfo user = new UserPersonalInfo();
         Scanner input1 = new Scanner(System.in);
         Scanner input2 = new Scanner(System.in);
         Scanner input3 = new Scanner(System.in);
         Scanner input4 = new Scanner(System.in);
-        Admin admin = new Admin(user);
         Scanner input = new Scanner(System.in);
         int choice = input.nextInt();
         while (true) {
 
             switch (choice) {
-                case 1:
+                case 1://admin
                     System.out.print("enter your name :");
                     String userName = input1.next();
                     System.out.print("enter your password :");
@@ -31,53 +44,42 @@ public class Arrive_Me_There {
                     String email = input3.next();
                     System.out.print("enter your phone number :");
                     String phoneNumber = input4.next();
-
                     user.createUserInfoWithEmail(userName, password, email, phoneNumber);
+                    Admin admin = new Admin(user);
                     admin.addNewAdmin(user);
                     break;
-                case 2:
+                case 2://client
                     System.out.print("enter your name :");
-
                     userName = input1.next();
                     System.out.print("enter your password :");
-
                     password = input2.next();
                     System.out.print("enter your email :");
-
                     email = input3.next();
                     System.out.print("enter your phone number :");
-
                     phoneNumber = input4.next();
                     System.out.println("1- requset ride\n"
                             + "2- offers \n"
                             + "3- finish ride \n"
                             + "4- logout");
                     user.createUserInfoWithEmail(userName, password, email, phoneNumber);
-                    //client
                     break;
-                case 3:
+                case 3://driver
                     System.out.print("enter your name :");
-
                     userName = input1.next();
                     System.out.print("enter your password :");
-
                     password = input2.next();
                     System.out.print("enter your email :");
-
                     email = input3.next();
                     System.out.print("enter your phone number :");
-
                     phoneNumber = input4.next();
                     System.out.println("1- add favourite areas\n"
                             + "2- available trips\n"
                             + "3- show users rate\n"
                             + "4- logout");
                     user.createUserInfoWithEmail(userName, password, email, phoneNumber);
-                    //driver
                     break;
                 case 4:
                     exit(0);
-                    break;
 
             }
         }
