e39a16680b3819b27c4cd14d026be07d58b90647
Mo7amed676 <84755046+Mo7amed676@users.noreply.github.com>
2021-12-07 00:14:10 +0200

    change an error in this file

--- Arrive_Me_There/src/arrive_me_there/Arrive_Me_There.java
+++ Arrive_Me_There/src/arrive_me_there/Arrive_Me_There.java
@@ -11,6 +11,7 @@ public class Arrive_Me_There {
 
     public static void main(String[] args) {
         DB connection = DB.getInstance();
+        Stranger guy=new Stranger();
         System.out.println("Login For Initial Admin.");
 
         Scanner inp1 = new Scanner(System.in);
@@ -25,13 +26,13 @@ public class Arrive_Me_There {
         String email1 = inp3.next();
         System.out.print("enter your phone number :");
         String phoneNumber1 = inp4.next();
-        UserPersonalInfo adminUser = new UserPersonalInfo();
-        adminUser.createUserInfoWithEmail(userName1, password1, email1, phoneNumber1);
+        UserPersonalInfo adminUser = UserPersonalInfo.createUserInfoWithEmail(userName1, phoneNumber1, email1, password1);
+        //adminUser.createUserInfoWithEmail(userName1, password1, email1, phoneNumber1);
 
         Admin mainAdmin = new Admin(adminUser);
         connection.addAdmin(mainAdmin);
         System.out.println("Login as 1-Add New Admin\n         2-Client\n         3-Driver\n         4-exit");
-        UserPersonalInfo user = new UserPersonalInfo();
+//        UserPersonalInfo user = UserPersonalInfo.createUserInfoWithEmail(userName1, phoneNumber1, email1, password1);
         Scanner input1 = new Scanner(System.in);
         Scanner input2 = new Scanner(System.in);
         Scanner input3 = new Scanner(System.in);
@@ -53,9 +54,9 @@ public class Arrive_Me_There {
                     String email = input3.next();
                     System.out.print("enter your phone number :");
                     String phoneNumber = input4.next();
-                    user.createUserInfoWithEmail(userName, password, email, phoneNumber);
-                    Admin admin = new Admin(user);
-                    admin.addNewAdmin(user);
+                    UserPersonalInfo.createUserInfoWithEmail(userName, password, email, phoneNumber);
+                    //Admin admin = new Admin(user);
+                    //admin.addNewAdmin(user);
                     break;
                 case 2://client
                     System.out.print("enter your name :");
@@ -70,8 +71,9 @@ public class Arrive_Me_There {
                             + "2- offers \n"
                             + "3- finish ride \n"
                             + "4- logout");
-                    user.createUserInfoWithEmail(userName, password, email, phoneNumber);
-                    Client client1=new Client(user);
+                    UserPersonalInfo.createUserInfoWithEmail(userName, password, email, phoneNumber);
+                    //Client client1=new Client(user);
+                    //guy.registerAsClient(user);
                     break;
                 case 3://driver
                     System.out.print("enter your name :");
@@ -86,12 +88,13 @@ public class Arrive_Me_There {
                     String drivingLicense = input5.next();
                     System.out.print("enter your phone number :");
                     String nationalID = input6.next();
-                    Driver driver1=new Driver(user, drivingLicense, nationalID);
+                    Driver driver1=new Driver(UserPersonalInfo.createUserInfoWithEmail(userName, phoneNumber, email, password), drivingLicense, nationalID);
                     System.out.println("1- add favourite areas\n"
                             + "2- available trips\n"
                             + "3- show users rate\n"
                             + "4- logout");
-                    user.createUserInfoWithEmail(userName, password, email, phoneNumber);
+                    UserPersonalInfo.createUserInfoWithEmail(userName, password, email, phoneNumber);
+//                    guy.registerAsDriver(user, drivingLicense, nationalID);
                     break;
                 case 4:
                     exit(0);
